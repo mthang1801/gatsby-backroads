7719475dd84cf50b0c79e4b0f7384c2898bbcbc5
mthang1801 <mthang1801@gmail.com>
2020-12-10 11:01:00 +0700

    change site name at gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,18 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 require("dotenv").config({
-  path : ".env"
+  path: ".env",
 })
 module.exports = {
   /* Your site config here */
-  siteMetadata : {
-    title : "Backroad", 
-    description : "Exploring awesome world wide tours and discover what makes each of them unique.",
-    author : "MVT",    
-    image : "/hawaii1.jpg",
-    twitterUsername : "@mvt",
-    siteUrl : "https://travelling-project.netlify.app"
+  siteMetadata: {
+    title: "Backroad",
+    description:
+      "Exploring awesome world wide tours and discover what makes each of them unique.",
+    author: "MVT",
+    image: "/hawaii1.jpg",
+    twitterUsername: "@mvt",
+    siteUrl: "https://travelling-project.netlify.app",
   },
   plugins: [
     {
@@ -34,12 +35,12 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve : `gatsby-plugin-styled-components`      
+      resolve: `gatsby-plugin-styled-components`,
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,       
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
@@ -48,12 +49,18 @@ module.exports = {
     `gatsby-plugin-playground`,
     `gatsby-plugin-sitemap`,
     {
-      resolve : `gatsby-plugin-robots-txt` ,
+      resolve: `gatsby-plugin-robots-txt`,
       options: {
-        host: 'https://epic-bartik-aa2a36.netlify.app',
-        sitemap: 'https://epic-bartik-aa2a36.netlify.app/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    }
+        host: "https://epic-bartik-aa2a36.netlify.app",
+        sitemap: "https://epic-bartik-aa2a36.netlify.app/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-disqus`,
+      options: {
+        shortname: `https-travelling-project-netlify-app-disqus`,
+      },
+    },
   ],
 }
